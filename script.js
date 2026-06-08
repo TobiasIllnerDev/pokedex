6f8ec1551e72202125cb40bc41afb1a6b224c240
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-08 15:41:55 +0200

    feat: fetch api

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const searchInput = document.querySelector("#search-input");
 const numberFilter = document.querySelector("#numbert");
 const nameFilter = document.querySelector("#name");
 const notFoundMessage = document.querySelector("#not-found-message");
+
+let allPokemons = [];
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`).then((response) => response.json()).then((data) => {
+    allPokemons = data.results;
+})
+
+
+async function fetchPokemonDataBeforeRedirect(id) {
+    try {
+        const [pokemon, pokemonSpecies] = await Promise.all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => {
+            res.json()
+        })])
+    }
+
+}
