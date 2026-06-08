55413eaf45dc01f43c85f58d6f48cf890018a1ae
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-08 15:19:59 +0200

    feat: added const for read documents and max-pokemon

---
+++ script.js
@@ -0,0 +1,6 @@
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#numbert");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
